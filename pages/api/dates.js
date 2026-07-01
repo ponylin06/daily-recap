280c04dd0b59e30c243cbb7b9d84a64ffc26fd0f
ponylin06 <2025020116@mails.szu.edu.cn>
2026-07-01 12:19:17 +0800

    Update dates.js

--- pages/api/dates.js
+++ pages/api/dates.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   const dir = path.join(process.cwd(), 'data')
   try {
     const files = fs.readdirSync(dir)
-      .filter(f => f.endsWith('.json'))
+      .filter(f => f.endsWith('.json') && f.startsWith('202'))
       .map(f => f.replace('.json', ''))
       .sort()
     res.status(200).json(files)
